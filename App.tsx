852627521f46999b35de2b8ed2982f5feea19920
Nikolai Konushkov <nkonushkov@mail.ru>
2021-10-16 21:58:58 +0300

    actualization webview props

--- App.tsx
+++ App.tsx
@@ -6,10 +6,17 @@ import {
 import { WebView } from 'react-native-webview';
 
 const App = () => {
+  const onMessage = (data: any) => { }
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
       <StatusBar hidden />
-      <WebView source={{uri: 'ya.ru'}} />
+      <WebView source={{ uri: 'https://wiildwood.online/events' }}
+        geolocationEnabled={true}
+        onMessage={onMessage}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        userAgent={"Chrome/56.0.0.0 Mobile"} />
     </SafeAreaView>
   );
 }
