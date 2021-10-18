6643597f606647a3a0b3a5c25f0c3babc442ec03
Nikolai Konushkov <nkonushkov@mail.ru>
2021-10-18 16:12:48 +0300

    handler backbutton add

--- App.tsx
+++ App.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import {
+  BackHandler,
   SafeAreaView,
   StatusBar,
 } from 'react-native';
 import { WebView } from 'react-native-webview';
-import SplashScreen from  'react-native-splash-screen';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
   const onMessage = (data: any) => { }
+  const webview = React.useRef<WebView>(null);
 
   React.useEffect(() => {
     SplashScreen.hide();
   });
+
+  React.useEffect(() => {
+    const backAction = () => {
+      if (webview.current) {
+        webview.current.goBack();
+        return true;
+      }
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => backHandler.remove();
+  }, []);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -21,7 +39,10 @@ const App = () => {
         onMessage={onMessage}
         javaScriptEnabled={true}
         domStorageEnabled={true}
-        userAgent={"Chrome/56.0.0.0 Mobile"} />
+        userAgent={"Chrome/56.0.0.0 Mobile"}
+        allowsBackForwardNavigationGestures={true}
+        ref={webview}
+      />
     </SafeAreaView>
   );
 }
